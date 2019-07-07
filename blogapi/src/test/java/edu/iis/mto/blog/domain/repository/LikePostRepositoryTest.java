017d5e9c76d9fa7e5faba68f3ae6e8e4efb38d3b
Jakub Mroczek <jakubmroczek2@gmail.com>
2019-07-07 15:26:47 +0200

    test for getting list of likes

--- blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikePostRepositoryTest.java
+++ blogapi/src/test/java/edu/iis/mto/blog/domain/repository/LikePostRepositoryTest.java
@@ -1,11 +1,16 @@
 package edu.iis.mto.blog.domain.repository;
 
 import static edu.iis.mto.blog.domain.model.AccountStatus.NEW;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.hasSize;
+import static org.junit.Assert.assertThat;
 
 import edu.iis.mto.blog.domain.model.BlogPost;
 import edu.iis.mto.blog.domain.model.LikePost;
 import edu.iis.mto.blog.domain.model.User;
+import java.util.List;
 import org.junit.Before;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
@@ -56,5 +61,14 @@ public class LikePostRepositoryTest {
     like.setUser(user);
     like.setPost(post);
   }
-  
+
+  @Test
+  public void savedLikeShouldBeInList() {
+    repository.save(like);
+
+    List<LikePost> returnedLikes = repository.findAll();
+
+    assertThat(returnedLikes, hasSize(1));
+    assertThat(returnedLikes, contains(like));
+  }
 }
